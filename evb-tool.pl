dc559a4e1bce6b6c22df6b34fbd97a128228f275
Kamen Naydenov <pau4o@kamennn.eu>
2012-06-15 10:19:59 +0300

    refactor and improvements
    - gather information for VirtualBox machine in one step - big speed improvement.
    - friendly exit message
    - exit on any key not listed at row with VirtualBox machine name
    - process 'paused' state - we can 'Resume'  from it

--- evb-tool.pl
+++ evb-tool.pl
@@ -7,7 +7,7 @@ use warnings;
 my %vbox = (
   manager  => 'VBoxManage',
 );
-my (%ebox, %vms, %what);
+my (%ebox, @vms, %what);
 
 # check for needed commands and give their full path
 foreach (keys %vbox) {
@@ -21,50 +21,58 @@ foreach (keys %vbox) {
 }
 ### %ebox
 
-# get list of VirtualBox machines
-open(my $machines, '-|', "$ebox{manager} list vms")
+# get list of VirtualBox machines and their info
+open(my $machines, '-|', "$ebox{manager} list -l vms")
   or die "Can't run $ebox{manager}: $!\n";
-while (<$machines>) {
-  chomp;
-  my ($name, $uuid) = split;
-  $uuid =~ s/[{}]//g;
-  $vms{$uuid}{name} = $name;
+{
+  # hack to split machine info after state
+  local $/ = 'Monitor count';
+  while (<$machines>) {
+    my @info = split /\n/;
+    my $vm_info;
+    foreach (@info) {
+      next unless m/^(Name|Guest OS|UUID|State):\s+([\w\s.-]+)/;
+      $vm_info->{$1} = $2;
+    }
+    ### VM Info: $vm_info
+    if ($vm_info) {
+      push @vms, $vm_info;
+    }
+  }
 }
 close $machines or die "$!\n";
-
-# get state of VirtualBox machines
-foreach my $uuid (keys %vms) {
-  open(my $state, '-|', "$ebox{manager} showvminfo $uuid --machinereadable")
-    or die "Can't run $ebox{manager}: $!\n";
-  while (<$state>) {
-    chomp;
-    next unless /^(VMState|GuestOSType)="(\w+)"/;
-    $vms{$uuid}{$1} = $2;
-  }
-  close $state or die "$!\n";
-}
-### %vms
+### @vms
 # Show menu with commands
 system("clear");
 
-my $i;
-print "0. exit\n";
-$what{0} = '';
-foreach my $uuid (keys %vms) {
-  if ($vms{$uuid}{VMState} =~ /(saved|poweroff)/) {
-    my $action = 'Run';
-    $action = 'Resume' if $1 eq 'saved';
-    my $type = $vms{$uuid}{GuestOSType} =~ /windows/i ? 'gui' : 'headless';
-    print ++$i, '. ', $action, "\t",  $vms{$uuid}{name}, "\n";
-    $what{$i} = qq($ebox{manager} startvm $uuid --type $type);
-  }
-  elsif ($vms{$uuid}{VMState} =~ /running/) {
-    print ++$i, '. Save ', "\t", $vms{$uuid}{name}, "\n";
-    $what{$i} = qq($ebox{manager} controlvm $uuid savestate);
+print "q. Exit\n";
+foreach my $index ( 0 .. $#vms) {
+  my %vbox = %{$vms[$index]};
+  foreach (keys %vbox) {
+    next unless m/State/;
+    my $action = 'Run   ';
+    if ( $vbox{State} =~ /(saved|poweroff)/) {
+      $action = 'Wake  ' if $1 eq 'saved';
+      my $type = $vbox{'Guest OS'} =~ /windows/i ? 'gui' : 'headless';
+      $what{$index} = qq($ebox{manager} startvm $vbox{UUID} --type $type);
+    } elsif ($vbox{State} =~ /running/) {
+      $action = 'Save  ';
+      $what{$index} = qq($ebox{manager} controlvm $vbox{UUID} savestate);
+    }
+    elsif ($vbox{State} =~ /paused/) {
+      $action = 'Resume';
+      $what{$index} = qq($ebox{manager} controlvm $vbox{UUID} resume);
+    }
+    else {
+      $index = '#';
+      $action = $vbox{State};
+    }
+    print qq($index. $action\t$vbox{Name}\n);
   }
 }
 ### %what
 print "Please choose action: ";
 my $answer = <STDIN>;
 chomp $answer;
-`$what{$answer}`;
+`$what{$answer}` if exists $what{$answer};
+print qq(See you later\n);
